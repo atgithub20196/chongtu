88e0bac669196c87bc539ab8a92257e5ccfba96a
tom-glb <goodMorning_glb@atguigu.com>
2019-06-21 23:49:08 +0800

    添加项目文件。

---
+++ 冲突吗/冲突吗.cpp
@@ -0,0 +1,16 @@
+﻿// 冲突吗.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+union U {
+	char str[2];
+	short int num;
+};
+int main()
+{
+	U u;
+	u.num = 266;
+    std::cout << "Hello World!\n"; 
+}
+
